c47cfb70c959cbb26713d001402ed983cf2e2760
GerVaf <thantzinmin667@gmail.com>
2023-04-15 13:02:27 +0630

    version1NonResponsive

---
+++ src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Foot from "./Footer/Foot";
+import Customer from "./Home/Customer";
+import HeroLeft from "./Home/HeroLeft";
+import HeroRight from "./Home/HeroRight";
+import Lang_Pj from "./Home/Lang_Pj";
+import Path from "./Home/Path";
+
+const Home = () => {
+  return (
+    <>
+      <div className="flex">
+        <HeroLeft />
+        <HeroRight />
+      </div>
+      <div>
+        <Lang_Pj/>
+      </div>
+      <Customer/>
+      <Path/>
+      <Foot/>
+    </>
+  );
+};
+
+export default Home;
